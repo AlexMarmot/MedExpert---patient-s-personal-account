7e085d435c42507ba1b0b6d53edd401ea38c9adb
SurkovAleshka <baks123321@yandex.ru>
2023-08-07 22:52:41 +0300

    Создал модальное окно с валидацией

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+//Модальное окно
 const myModal = new HystModal({
     linkAttributeName: "data-hystmodal",
-    // настройки (не обязательно), см. API
 });
+
+//Модальное окно
+function validateForm() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    
+    if (password !== confirmPassword) {
+        alert("Пароль и подтверждение пароля не совпадают");
+        return false;
+    }
+    
+    var inputs = document.forms["registrationForm"].getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].checkValidity() === false) {
+            alert("Пожалуйста, заполните все поля корректно");
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+function togglePasswordVisibility(inputId) {
+    var input = document.getElementById(inputId);
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
